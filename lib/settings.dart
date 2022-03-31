29fe5d0f3d6b513edaf47e56c7225bc963ec5130
Louis Gallet <louis.gallet@tumo.world>
2022-03-31 22:01:50 +0200

    feat: :construction:  Starting basic UI

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
   runApp(const Settings());
@@ -12,20 +13,33 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  Future<PackageInfo> _getPackageInfo() {
+    return PackageInfo.fromPlatform();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
-        Text('Settings'),
-        TextButton(
-          onPressed: null,
-          child: Text('Version 0.0.1'),
-        )
-      ],
-    )));
+            child: FutureBuilder<PackageInfo>(
+                future: _getPackageInfo(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<PackageInfo> snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("ERROR");
+                  } else if (!snapshot.hasData) {
+                    return const Text("Loading...");
+                  }
+                  final data = snapshot.data!;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('App Name: ${data.appName}'),
+                      Text('Package Name: ${data.packageName}'),
+                      Text('Version: ${data.version}'),
+                      Text('Build Number: ${data.buildNumber}'),
+                    ],
+                  );
+                })));
   }
 }
