1a0f9391c94881aecaf9f0cbe5869bd03d72fc07
Louis Gallet <louis.gallet@tumo.world>
2022-05-22 16:21:45 +0200

    feat: :construction:  starting textinput

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, TextInput } from 'react-native';
 import { Text, View } from '../components/Themed';
-import { TextField } from 'react-native-ios-kit';
+import React from 'react'
 
 export default function TabTwoScreen() {
+
+  const [text, setText] = React.useState('');
+ 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Settings</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
+      <TextInput placeholder='XBackBones API Key' onChangeText={text => setText(text)}></TextInput>
     </View>
   );
 }
@@ -29,3 +31,4 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
