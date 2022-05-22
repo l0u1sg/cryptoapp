c0fff5db7a38b0efb18066abf5e9c743b9425091
Louis Gallet <louis.gallet@tumo.world>
2022-05-22 13:03:53 +0200

    feat: :heavy_plus_sign:  Add ios kit design

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { TextField } from 'react-native-ios-kit';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Settings</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      
     </View>
   );
 }
